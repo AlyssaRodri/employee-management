c44ea40f3c372dd803bcc54ad306e034506d9eb2
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-04-08 12:48:26 -0500

    Seed sql started.

--- db/seed.sql
+++ db/seed.sql
@@ -1,14 +1,21 @@
 INSERT INTO department (dept_name)
 VALUES  ("Human Resources"),
-        ("Marketing"),
+        ("Legal"),
         ("Accounting"),
         ("Sales"),
         ("General Management");
 
 
 INSERT INTO roles (title, salary, dept_id)
-VALUES  ("HR Director", 200000, 1),
+VALUES  ("HR Director", 60000, 1),
         ("Junior Sales Associate", 40000, 4),
-        ("General Manager", 45000, 5);
+        ("General Manager", 45000, 5),
+        ("Payroll Generalist", 50000, 3),
+        ("Lawyer", 120000, 2),
+        ("Paralegal", 45000, 2)
 
-INSERT INTO employee
+INSERT INTO employee ((first_name, last_name, role_id, manager_id)
+VALUES  ("Gary", "Almes", 3, null),
+        ("Alyssa", "Rodriguez", 1, 1)
+        ("Molly", "Landon", 5, null),
+        ("Jessica", "Brown", 6, 3)
